52b0d80bb9ef7e6b1d195db58c4acf862932f55b
ashimumam4513-stack <ashimumam4513@gmail.com>
2026-08-28 14:21:35 +0700

    chore: add build.sh for vercel deployment

---
+++ thoyyiba_app/build.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+# Install Flutter
+echo "Downloading Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+# Add flutter to path
+export PATH="$PATH:$PWD/flutter/bin"
+
+echo "Flutter version:"
+flutter --version
+
+echo "Building web app..."
+flutter clean
+flutter pub get
+flutter build web
